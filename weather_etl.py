1e09e8f4655d2f3c3603ce95309c4b6963a8b6a1
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-01-10 14:00:46 +0000

    saves the current weather data to csv

--- weather_etl.py
+++ weather_etl.py
@@ -1,34 +1,42 @@
 import requests
 import pandas as pd
+import datetime as dt
+
+weather_data_list = [] 
+
+def get_weather_data():
+
+    #api_url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=775ce95223886fd4d68d7401358687d5"
+    api_url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=775ce95223886fd4d68d7401358687d5"
+    #Make the API call
+    response = requests.get(api_url)
+
+    #Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
 
 
-#api_url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=775ce95223886fd4d68d7401358687d5"
-api_url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=775ce95223886fd4d68d7401358687d5"
-#Make the API call
-response = requests.get(api_url)
 
-#Check if the request was successful
-if response.status_code == 200:
-    data = response.json()
+    #Extract data and store in a DataFrame
+    
+    weather_data = {
+    'id': data['id'],   
+    'city':data['name'],
+    'timezone': data['timezone'],
+    'temperature':data['main']['temp'],
+    'humidity':data['main']['humidity'],
+    'windspeed': data['wind']['speed'],
+    'date':dt.datetime.utcfromtimestamp(data['dt']).strftime("%Y-%m-%d"),
+    'time': dt.datetime.utcfromtimestamp(data['dt']).strftime("%H:%M:%S")
+    }
 
+    weather_data_list.append(weather_data)
 
-for x,y in data.items():
-    print(x,y)
-#Extract data and store in a DataFrame
-weather_data = {
-'id': data['id'],   
-'city':data['name'],
-'timezone': data['timezone'],
-'temperature':data['main']['temp'],
-'humidity':data['main']['humidity'],
-'windspeed': data['wind']['speed'],
-'timestamp': data['dt']
+    df = pd.DataFrame(weather_data_list)
 
-}
-
-df = pd.DataFrame([weather_data])
-
-print(df)
+    print(df)
+    df.to_csv('current_weather_data.csv')
     
     
     
+get_weather_data()
