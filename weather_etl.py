590507369b08c3391f6b17d8c520192d7b543a4d
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-01-10 13:22:32 +0000

    made api call and saved to df

--- weather_etl.py
+++ weather_etl.py
@@ -1,8 +1,9 @@
 import requests
-import pandas as pandas
+import pandas as pd
 
-api_url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=775ce95223886fd4d68d7401358687d5"
 
+#api_url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=775ce95223886fd4d68d7401358687d5"
+api_url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=775ce95223886fd4d68d7401358687d5"
 #Make the API call
 response = requests.get(api_url)
 
@@ -11,4 +12,23 @@ if response.status_code == 200:
     data = response.json()
 
 
-print(data)
+for x,y in data.items():
+    print(x,y)
+#Extract data and store in a DataFrame
+weather_data = {
+'id': data['id'],   
+'city':data['name'],
+'timezone': data['timezone'],
+'temperature':data['main']['temp'],
+'humidity':data['main']['humidity'],
+'windspeed': data['wind']['speed'],
+'timestamp': data['dt']
+
+}
+
+df = pd.DataFrame([weather_data])
+
+print(df)
+    
+    
+    
